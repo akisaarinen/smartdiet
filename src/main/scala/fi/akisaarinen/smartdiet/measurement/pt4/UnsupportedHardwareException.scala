c04820a3245848ce76db20d4de77e88f97269f76
Aki Saarinen <aki@akisaarinen.fi>
2011-11-02 12:43:14 +0200

    Add licences to scala files

--- src/main/scala/fi/akisaarinen/smartdiet/measurement/pt4/UnsupportedHardwareException.scala
+++ src/main/scala/fi/akisaarinen/smartdiet/measurement/pt4/UnsupportedHardwareException.scala
@@ -1,3 +1,26 @@
+/*
+ * This file is part of SmartDiet.
+ *
+ * Copyright (C) 2011, Aki Saarinen.
+ *
+ * SmartDiet was developed in affiliation with Aalto University School
+ * of Science, Department of Computer Science and Engineering. For
+ * more information about the department, see <http://cse.aalto.fi/>.
+ *
+ * SmartDiet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SmartDiet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SmartDiet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fi.akisaarinen.smartdiet.measurement.pt4
 
 class UnsupportedHardwareException(reason: String) extends Exception(reason)
